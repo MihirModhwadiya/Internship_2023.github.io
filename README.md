2aca67509be1ef775553936df2b4c8c8284221b1
MewMihStow3339 <mihirmodhwadiya21@gmail.com>
2023-08-01 12:03:45 +0530

    chintanbhai

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Internship_2023.github.io
+
+hi this is our first ptoject
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*!
     * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
     * Copyright 2013-2023 Start Bootstrap
@@ -24,3 +25,31 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+=======
+/*!
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+>>>>>>> intern/shubham
